8c0f3aee06971103d2e8f15a5fba4824798ea72b
thobielorbah <koya.oluwatobi@gmail.com>
2018-04-16 13:59:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HNG INTERNSHIP</title>
+		<link href="https://fonts.googleapis.com/css?family=Dosis|Raleway:300,400,800|Orbitron" rel="stylesheet"> 
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		
+	</head>
+
+	<body>
+	
+	 <div id="container">
+	 	<div id="header">
+	 		
+	 	</div>
+	 	<div id="content">
+	 	
+	 		<div id="main">
+	 			<h1 class="heading">HNG</h1>
+	 			<p class="subheading">Internship 4.0</p>
+	 			<?php
+					//echo date_default_timezone_set('Africa/Algiers');
+					echo date("h:i:sa"). "<br>";
+				 ?>
+	 		</div>
+	 		<div id="image">
+	 			<img src="img/pc-vector.png">
+	 		</div>
+	 	</div>
+	 	<div id="footer">
+	 		<p class="foot">@thobielorbah</p>
+	 	</div>
+	 	
+	 </div>
+	</body>
+</html>
